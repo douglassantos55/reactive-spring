20fc7020759eed17549593362f2920ee328b9438
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-09-04 13:53:46 -0300

    feat(orders): restaurant message propagation

--- orders/src/main/java/br/com/ftgo/orders/event/handler/RestaurantEventsHandler.java
+++ orders/src/main/java/br/com/ftgo/orders/event/handler/RestaurantEventsHandler.java
@@ -1,19 +1,26 @@
 package br.com.ftgo.orders.event.handler;
 
 import br.com.ftgo.orders.entity.Restaurant;
+import br.com.ftgo.orders.event.ContextHandler;
 import br.com.ftgo.orders.repository.RestaurantsRepository;
 import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestaurantEventsHandler {
-    @Autowired
     private RestaurantsRepository repository;
+
+    private ContextHandler contextHandler;
+
+    public RestaurantEventsHandler(RestaurantsRepository repository, ContextHandler contextHandler) {
+        this.repository = repository;
+        this.contextHandler = contextHandler;
+    }
 
     @RabbitListener(bindings = {
             @QueueBinding(
@@ -27,8 +34,13 @@ public class RestaurantEventsHandler {
                     key = "restaurant.updated"
             ),
     })
-    public void handleCreatedUpdated(Restaurant restaurant) {
-        repository.save(restaurant).block();
+    public void handleCreatedUpdated(Restaurant restaurant, Message message) {
+        contextHandler.withMessageContext(message, span ->
+                repository
+                        .save(restaurant)
+                        .doOnError(exception -> span.recordException(exception))
+                        .block()
+        );
     }
 
     @RabbitListener(bindings = @QueueBinding(
@@ -36,7 +48,12 @@ public class RestaurantEventsHandler {
             exchange = @Exchange(name = "notifications.exchange", type = ExchangeTypes.TOPIC),
             key = "restaurant.deleted"
     ))
-    public void handleDeleted(Restaurant restaurant) {
-        repository.delete(restaurant).block();
+    public void handleDeleted(Restaurant restaurant, Message message) {
+        contextHandler.withMessageContext(message, span ->
+                repository
+                        .delete(restaurant)
+                        .doOnError(exception -> span.recordException(exception))
+                        .block()
+        );
     }
 }
